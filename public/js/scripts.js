f1dc3e1c3488d70a2e4757bc9c47e162c49b6d98
Jasj0t <jasjotcobra@gmail.com>
2022-12-03 18:32:28 +1100

    Practical 3

---
+++ public/js/scripts.js
@@ -0,0 +1,53 @@
+const cardList = [
+        {
+            title: "Meta 2",
+            image: "images/index1.jpg",
+            link: "About Meta 2",
+            desciption: "Demo desciption about meta 2"
+        },
+        {
+            title: "Meta 3",
+            image: "images/index.jpg",
+            link: "About Meta 3",
+            desciption: "Demo desciption about meta 3"
+        }
+    ]
+    const clickMe = () => {
+        alert("This is a demo to week 3 practical")
+    }
+    
+    const submitForm = () => {
+        let formData = {};
+        formData.first_name = $('#first_name').val();
+        formData.last_name = $('#last_name').val();
+        formData.password = $('#password').val();
+        formData.email = $('#email').val();
+    
+        console.log("Form Data Submitted: ", formData);
+    }
+    
+    const addCards = (items) => {
+        items.forEach(item => {
+            let itemToAppend = '<div class="col s4 center-align">'+
+        '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+        '</div><div class="card-content">'+
+        '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+        '<div class="card-reveal">'+
+            '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+            '<p class="card-text">'+item.desciption+'</p>'+
+          '</div></div></div>';
+          $("#card-section").append(itemToAppend)
+        });
+    }
+    
+    
+    
+    $(document).ready(function(){
+        $('.materialboxed').materialbox();
+        $('#formSubmit').click(()=>{
+            submitForm();
+        })
+        addCards(cardList);
+        $('.modal').modal();
+      });
+    
